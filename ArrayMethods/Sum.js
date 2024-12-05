e942ce321d4305efd02d248d357ca7070c8fbd40
Kirti11 <kirtijhala1110@gmail.com>
2024-12-05 23:26:40 +0530

    Create Sum.js

---
+++ ArrayMethods/Sum.js
@@ -0,0 +1,10 @@
+/*  2.If the given input is an array of numbers, return the sum of all the positives ones. If the array is empty or there aren't any positive numbers, return 0.
+
+const input = [1, -4, 12, 0, -3, 29, -150];
+output :- 42;   */
+
+
+const input = [1,2,3,4];
+
+let arr2 = input.reduce((sum, x)=>  sum+x,0);
+console.log(arr2);
