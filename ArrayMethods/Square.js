11c14f6d373e4c2caf781921562591de3694bf64
Kirti11 <kirtijhala1110@gmail.com>
2024-12-05 23:26:11 +0530

    Create Square.js

---
+++ ArrayMethods/Square.js
@@ -0,0 +1,7 @@
+/* 1. Square the value of every element in the array. Presume that you will only get numbers in the input array.
+ const input = [1, 2, 3, 4, 5];
+ output: - [1, 4, 9, 16, 25];       */
+
+ let arr = [1, 2, 3, 4, 5];
+ let arr2 = arr.map((a) => a*a);
+ console.log(arr2);
